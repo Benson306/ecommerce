533f5577eefcc2ebf81743f9626c4639f1655f63
Benson Ndiwa <bnkimtai@gmail.com>
2022-06-20 17:07:27 +0300

    typos corrected

--- src/products/Products.js
+++ src/products/Products.js
@@ -72,9 +72,9 @@ const Products = () => {
    <br />
             <div className="topButton">
 
-                <Link to="/admin_dashboard/add_products" style={{display:'flex', width:'15%',border: '1px solid maroon',marginLeft: '3%',textDecoration: 'none', backgroundColor: 'orange', color:'black', padding: '10px'}}>
+                <Link to="/admin_dashboard/add_products" style={{display:'flex', width:'16%',border: '1px solid maroon',marginLeft: '3%',textDecoration: 'none', backgroundColor: 'orange', color:'black', padding: '10px'}}>
                     <img src={ require('../images/add.png')} alt="" width="22px" style={{marginRight:'6%'}}/>
-                    Add New Products
+                    Add A New Product
                 </Link>
             </div>
             <br />
