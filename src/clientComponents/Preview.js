9909d58ef179ae727066b25f69b78f04abed523a
Benson Ndiwa <bnkimtai@gmail.com>
2022-07-10 17:24:17 +0300

    preview get from db

--- src/clientComponents/Preview.js
+++ src/clientComponents/Preview.js
@@ -6,7 +6,7 @@ const Preview = () => {
 
     const [product, setProduct] = useState([]);
     const [pending , setPending] = useState(true);
-    const [error, setError] = useState(' ');
+    const [error, setError] = useState('');
 
     useEffect(()=>{
         const abortCont = new AbortController();
@@ -28,27 +28,47 @@ const Preview = () => {
             setPending(false);
         })
 
-        return () => abortCont.abort();
+        return () => abortCont.Abort();
 
     },[])
 
     
 
     return ( <div className="previewPage">
-        <div className="topPreview">
-            <div className="slideshow">
-                
-            </div>
-            <div className="delivery">
-                Delivery Details
-            </div>
-        </div>
+        { pending && <div>Loading ....</div> }
+        {!pending && <div className="topPreview">
+                <div className="slideshow">
+                    <div className="topArea">
+                        <div className="pics">
+                            <img src={require(`../uploads/${product.preview1}`)} width='200px' height='200px' style={{objectFit:'scale-down'}} alt="" />
+                            <br />
+                            <br />
+                            <div className="moreImages">
+                                <img src={require(`../uploads/${product.preview1}`)} width='50px' height='50px' style={{objectFit:'scale-down'}} alt="" />
+                                <img src={require(`../uploads/${product.preview2}`)} width='50px' height='50px' style={{objectFit:'scale-down'}} alt="" />
+                                <img src={require(`../uploads/${product.preview3}`)} width='50px' height='50px' style={{objectFit:'scale-down'}} alt="" />
+                                <img src={require(`../uploads/${product.preview4}`)} width='50px' height='50px' style={{objectFit:'scale-down'}} alt="" />
+                            </div>
+                        </div>
+                        <div className="topDetails">
+                            <div className="prodHeading">{product.prodName}</div>
+                            <div className="prodBody">Category: {product.categ}</div>
+                            <div className="prodFooter">Ksh. {product.price}</div>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="delivery">
+                    Delivery Details
+                </div>
+        </div>}
         <div className="detailsPreview">
             More details
         </div>
         <div className='moreProducts'>
             More Products
         </div>
+        
     </div> );
 }
 
