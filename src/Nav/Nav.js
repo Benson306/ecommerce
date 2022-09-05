c1c66936b1cff62a10e6227f361e47e76b03cc46
Benson Ndiwa <bnkimtai@gmail.com>
2022-09-05 11:55:19 +0300

    Nav styling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -12,16 +12,13 @@ const Nav = () => {
 
                 <li style={{fontSize:'22px',marginTop:'5px'}}>Ecomm</li>
 
-                <li style={{width:'50%'}}>
-                    
-                    
-                </li>
+                <li style={{width:'50%'}}></li>
 
-                <li style={{display:'flex'}}> <img src={require('../images/user.png')} width="25px" height="25px" style={{marginTop:'5%'}} alt="" /> <div style={{marginTop:'10%',marginLeft:'2%'}}><Link to="/login">Account</Link></div></li>
+                <li style={{display:'flex', justifyContent:'center', alignItems:'center'}}> <img src={require('../images/user.png')} width="25%" style={{marginTop:'5%', objectFit:'scale-down'}} alt="" /> <div style={{marginTop:'10%',marginLeft:'2%', paddingBottom:'5px'}}><Link to="/login" style={{color:'white', textDecoration:'none'}}>Account</Link></div></li>
 
-                <li style={{display:'flex'}}> <img src={require('../images/shopping-cart.png')} width="25px" height="30px" style={{marginTop:'5%'}} alt="" /> <div style={{marginTop:'10px',marginLeft:'5%'}}>Cart</div></li>
+                <li style={{display:'flex', justifyContent:'center', alignItems:'center'}}> <img src={require('../images/shopping-cart.png')} width="35%" style={{marginTop:'5%'}} alt="" /> <div style={{marginTop:'10px',marginLeft:'5%', paddingBottom:'5px'}}><Link to="/login" style={{color:'white', textDecoration:'none'}}>Cart</Link></div></li>
 
-                <li style={{display:'flex'}}> <img src={require('../images/love.png')} width="25px" height="25px" style={{marginTop:'10%'}} alt="" /> <div style={{marginTop:'12px',marginLeft:'5%'}}>Wishlist</div></li>
+                <li style={{display:'flex', justifyContent:'center', alignItems:'center'}}> <img src={require('../images/love.png')} width="22%" style={{marginTop:'10%'}} alt="" /> <div style={{marginTop:'12px',marginLeft:'5%', paddingBottom:'5px'}}><Link to="/login" style={{color:'white', textDecoration:'none'}}>WishList</Link></div></li>
             </ul>
         </nav>
     </div> );
